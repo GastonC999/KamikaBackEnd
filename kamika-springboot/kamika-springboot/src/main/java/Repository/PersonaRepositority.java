bef5336f4fc6ef41b1511c9e1f4a6e0e61707325
Gastón <86261180+GastonC999@users.noreply.github.com>
2022-09-26 14:30:50 -0300

    Delete kamika-springboot directory

--- kamika-springboot/kamika-springboot/src/main/java/Repository/PersonaRepositority.java
+++
@@ -1,9 +0,0 @@
-package Repository;
-
-import modelo.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonaRepositority extends JpaRepository<Persona, Integer> {
-}
